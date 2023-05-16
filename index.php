66975b52b528c81e23bcc478ff041d4e9aeaccd0
LQ-Wylp <73892402+LQ-Wylp@users.noreply.github.com>
2023-05-16 17:05:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-$con = mysqli_init();
-mysqli_real_connect($conn, "projetcloud-wdl-server.mysql.database.azure.com", "lmgnzkgwif", "0BV63Q3I6X140DXG$", "projetcloud-wdl-database", 3306, MYSQLI_CLIENT_SSL);
-
 echo("test");
 ?>
 
