037d2cbf7c0c4db0120f9c883e9f9c339d642268
LQ-Wylp <73892402+LQ-Wylp@users.noreply.github.com>
2023-05-16 17:07:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-
-echo("test");
+  echo("test");
 ?>
+
+<form method="post" action="">
+  <label for="nom">Nom:</label>
+  <input type="text" id="nom" name="nom"><br><br>
+
+  <label for="prenom">Prénom:</label>
+  <input type="prenom" id="prenom" name="prenom"><br><br>
+
+  <label for="texte">Texte:</label>
+  <textarea id="texte" name="texte"></textarea><br><br>
+
+  <input type="submit" value="Envoyer">
+</form>
 
